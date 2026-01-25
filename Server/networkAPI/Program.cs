ec979fbce3119eb5a6dd13fb60ddc16712ba7a31
Jaehyun9912 <limit8802@gmail.com>
2026-01-25 17:03:56 +0900

    Dotnet Compile Complete
    - protoc test compile

--- Server/networkAPI/Program.cs
+++ Server/networkAPI/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Protocol;
+
+var newMsg = new S_Login();
+
+newMsg.PlayerId = (int)PacketID.PktSLogin;
+newMsg.Success = true;
