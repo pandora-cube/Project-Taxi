e8c9654c17972c7ec283687a0852ba6fef885745
Jaehyun9912 <limit8802@gmail.com>
2026-02-01 22:40:54 +0900

    Set Server Entrypoint
    - Program Main 엔트리 구현
    - Socket Listen을 통해 Session 개방 임시 적용
    - dockerfile 적용

---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Protocol;
+
+namespace ServerCore;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. 엔드포인트 설정    
+        IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 7777);
+
+        // 2. 리스너 생성 및 초기화
+        Listener listener = new Listener();
+
+        // 접속이 발생했을 때 실행할 콜백 등록
+        listener.Init(endPoint, (clientSocket) => {
+            // 접속할 때마다 새로운 세션 객체 생성
+            Session session = new Session();
+            session.Start(clientSocket);
+        });
+
+        Console.WriteLine($"Echo Server is running...");
+        Console.WriteLine("Press 'quit' to exit.");
+
+        // 3. 서버 유지
+        while (true)
+        {
+            string? input = Console.ReadLine();
+            Console.WriteLine($"Get Input: {input}");
+            if (input == "quit") break;
+        }
+    }
+}
---
+++ Server/gameLogic/GameServer.cs
@@ -0,0 +1,25 @@
+﻿// Game Server Info
+
+using ServerCore;
+using Protocol;
+
+namespace GameLogic;
+
+public class Player
+{
+    public int playerID;
+    public string? nickname;
+    public List<Session> connects = [];
+
+    public float posX, posY, posZ;
+}
+
+public class GameServer
+{
+    public Player[] players = new Player[4];
+
+    public void RefreshPos(int playerID)
+    {
+
+    }
+}
